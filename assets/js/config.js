98d3d148e2c9c9879ee291698890b96025d88522
raul99-creater <dnjscjf0223@gmail.com>
2026-03-20 12:21:48 +0900

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
   supabaseUrl: 'https://pxmiohzuqoztnhablbfy.supabase.co',
-  supabasePublishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInJlZiI6InB4bWlvaHp1cW96dG5oYWJsYmZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NTEwNjQsImV4cCI6MjA4OTQyNzA2NH0.vK4ogWq0ksoa3NLFkJp-Z6ez7cszAoGXx68sipLgwC4',
+  supabasePublishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4bWlvaHp1cW96dG5oYWJsYmZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NTEwNjQsImV4cCI6MjA4OTQyNzA2NH0.vK4ogWq0ksoa3NLFkJp-Z6ez7cszAoGXx68sipLgwC4',
   siteName: '귤귤 일정관리',
   sessionStorageKey: 'gyulgyul_main_session_v1'
 };
